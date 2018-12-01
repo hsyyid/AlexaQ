611da30dc2802fb6f760bc546d7ced7b79f8c6f3
Hassan Syyid <hassansyyid@gmail.com>
2018-12-01 14:28:06 -0500

    Formatting

--- AlexaQ-API/index.js
+++ AlexaQ-API/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const spotify = require('./spotify');
 
-async function Play (position) {
+async function Play(position) {
     let devices = await spotify.GetDevices();
     let device_id = devices.filter(t => t.name === "Marko's Echo Dot")[0].id;
-    let {uri} = await spotify.GetPlaylist("AlexaQ Playlist");
+    let { uri } = await spotify.GetPlaylist("AlexaQ Playlist");
 
     await spotify.Play(device_id, uri, position);
 }
@@ -14,14 +14,14 @@ async function IsPlaying() {
     let currently_playing = await spotify.GetCurrentlyPlaying();
 
     return currently_playing && currently_playing.is_playing
-            && currently_playing.device && currently_playing.device.name === "Marko's Echo Dot"
-            && currently_playing.context && currently_playing.context.type && currently_playing.context.type === "playlist"
-                && currently_playing.context.uri === playlistUri;
+        && currently_playing.device && currently_playing.device.name === "Marko's Echo Dot"
+        && currently_playing.context && currently_playing.context.type && currently_playing.context.type === "playlist"
+        && currently_playing.context.uri === playlistUri;
 }
 
 module.exports = async function (context, req) {
     context.log(JSON.stringify(req, null, 2));
-    let {func} = req.query;
+    let { func } = req.query;
 
     switch (func) {
         case "Play": {
@@ -34,7 +34,7 @@ module.exports = async function (context, req) {
             break;
         }
         case "Search": {
-            let {term} = req.query;
+            let { term } = req.query;
             let results = await spotify.Search(term);
 
             context.res = {
@@ -45,7 +45,7 @@ module.exports = async function (context, req) {
             break;
         }
         case "Add": {
-            let {uris} = req.body;
+            let { uris } = req.body;
             await spotify.AddSongsToPlaylist(uris);
             let isPlaying = await IsPlaying();
 
@@ -66,8 +66,7 @@ module.exports = async function (context, req) {
         case "Queue": {
             let isPlaying = await IsPlaying();
 
-            if (isPlaying)
-            {
+            if (isPlaying) {
                 let tracks = await spotify.GetPlaylistTracks();
                 let curr_track = await spotify.GetCurrentTrack();
                 let position = tracks.findIndex(t => t.uri === curr_track.uri);
@@ -76,13 +75,12 @@ module.exports = async function (context, req) {
                 context.res = {
                     status: 200,
                     body: JSON.stringify({
-                      curr_track,
-                      tracks: tracks.slice(position)
+                        curr_track,
+                        tracks: tracks.slice(position)
                     })
                 };
             }
-            else
-            {
+            else {
                 context.res = {
                     status: 400,
                     body: JSON.stringify({
--- AlexaQ-API/spotify.js
+++ AlexaQ-API/spotify.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 const delay = require('delay');
-const {urlEncode} = require("./util.js");
+const { urlEncode } = require("./util.js");
 
 // const clientId = process.env.SPOTIFY_CLIENT_ID;
 // const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
@@ -16,40 +16,40 @@ const refresh_token = "***REMOVED***";
 * Gets a new access token to make requests
 */
 const RefreshToken = async () => {
-  let response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Authorization': 'Basic ' + new Buffer(`${clientId}:${clientSecret}`).toString('base64'),
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: urlEncode({refresh_token, grant_type: "refresh_token"})
-  });
+    let response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + new Buffer(`${clientId}:${clientSecret}`).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: urlEncode({ refresh_token, grant_type: "refresh_token" })
+    });
 
-  let json = await response.json();
-  console.log(JSON.stringify(json));
+    let json = await response.json();
+    console.log(JSON.stringify(json));
 
-  return json.access_token;
+    return json.access_token;
 };
 
 /**
 * Gets users playlist
 */
 const GetPlaylist = async (name) => {
-  let accessToken = await RefreshToken();
+    let accessToken = await RefreshToken();
 
-  let {id} = await GetUserProfile(accessToken);
-  let playlists = await GetList("https://api.spotify.com/v1/me/playlists?limit=50", {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json'
-    }
-  });
+    let { id } = await GetUserProfile(accessToken);
+    let playlists = await GetList("https://api.spotify.com/v1/me/playlists?limit=50", {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
+        }
+    });
 
-  console.log("Playlists: " + JSON.stringify(playlists));
+    console.log("Playlists: " + JSON.stringify(playlists));
 
-  return playlists.filter(p => p.owner.id === id && p.name === name)[0];
+    return playlists.filter(p => p.owner.id === id && p.name === name)[0];
 };
 
 /**
@@ -57,89 +57,89 @@ const GetPlaylist = async (name) => {
 * TODO: Queries seem iffy, sometimes using * is better, sometimes quotes, sometimes nothing... need to investigate further
 */
 const Search = async (term) => {
-  let accessToken = await RefreshToken();
-  let url = `https://api.spotify.com/v1/search?q=${term.toLowerCase()}*&type=track`;
+    let accessToken = await RefreshToken();
+    let url = `https://api.spotify.com/v1/search?q=${term.toLowerCase()}*&type=track`;
 
-  let response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json'
-    }
-  });
+    let response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
+        }
+    });
 
-  let data = await response.json();
-  return data.tracks.items;
+    let data = await response.json();
+    return data.tracks.items;
 };
 
 /**
 * Get playlist tracks
 */
 const GetPlaylistTracks = async () => {
-  let {id} = await GetPlaylist("AlexaQ Playlist");
-  let accessToken = await RefreshToken();
+    let { id } = await GetPlaylist("AlexaQ Playlist");
+    let accessToken = await RefreshToken();
 
-  let tracks = await GetList(`https://api.spotify.com/v1/playlists/${id}/tracks?limit=100`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json'
-    }
-  });
+    let tracks = await GetList(`https://api.spotify.com/v1/playlists/${id}/tracks?limit=100`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
+        }
+    });
 
-  return tracks.map(t => t.track);
+    return tracks.map(t => t.track);
 };
 
 /**
 * Gets current users profile
 */
 const GetUserProfile = async (accessToken) => {
-  let response = await fetch(`https://api.spotify.com/v1/me`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json'
-    }
-  });
+    let response = await fetch(`https://api.spotify.com/v1/me`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
+        }
+    });
 
-  return await response.json();
+    return await response.json();
 }
 
 /**
 * Adds specified songs to the playlist
 */
 const AddSongsToPlaylist = async (uris) => {
-  let accessToken = await RefreshToken();
-  let {id} = await GetPlaylist("AlexaQ Playlist");
+    let accessToken = await RefreshToken();
+    let { id } = await GetPlaylist("AlexaQ Playlist");
 
-  let response = await fetch(`https://api.spotify.com/v1/playlists/${id}/tracks`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({uris})
-  });
+    let response = await fetch(`https://api.spotify.com/v1/playlists/${id}/tracks`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ uris })
+    });
 
-  console.log("Result: [" + JSON.stringify(await response.json()) + "]");
+    console.log("Result: [" + JSON.stringify(await response.json()) + "]");
 };
 
 const GetDevices = async () => {
     let accessToken = await RefreshToken();
     console.log("AccessToken: " + accessToken);
-  
+
     let response = await fetch(`https://api.spotify.com/v1/me/player/devices`, {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-        'Accept': 'application/json'
-      }
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
+        }
     });
-  
+
     let data = await response.json();
     console.log(JSON.stringify(data, null, 2));
-  
+
     if (data.devices) {
         return data.devices;
     } else {
@@ -149,55 +149,55 @@ const GetDevices = async () => {
 
 const Play = async (device_id, playlist_uri, position) => {
     let accessToken = await RefreshToken();
-  
+
     let response = await fetch(`https://api.spotify.com/v1/me/player/play?device_id=${device_id}`, {
-      method: 'PUT',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`
-      },
-      body: JSON.stringify({
-        context_uri: playlist_uri,
-        offset: {
-           position
-        }
-      })
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        },
+        body: JSON.stringify({
+            context_uri: playlist_uri,
+            offset: {
+                position
+            }
+        })
     });
-  
+
     console.log(response.status);
 };
 
 const GetList = async (base_url, params) => {
     let list = [];
     let next = base_url;
-  
+
     do {
-      try {
-        let response = await fetch(next, params);
-  
-        if (response.status === 200) {
-          let data = await response.json();
-  
-          if (data && data.items && data.items.length > 0) {
-            list.push.apply(list, data.items);
-            next = data.next;
-            console.log(list.length + " / " + data.total);
-          }
-        } else if (response.status === 429 && response.headers.has("retry-after")) {
-          let retryDelay = parseInt(response.headers.get("retry-after"));
-          console.log(`Retrying after ${retryDelay} seconds...`);
-          // Delay is returned in seconds, we need it in millisec
-          await delay(retryDelay * 1000);
+        try {
+            let response = await fetch(next, params);
+
+            if (response.status === 200) {
+                let data = await response.json();
+
+                if (data && data.items && data.items.length > 0) {
+                    list.push.apply(list, data.items);
+                    next = data.next;
+                    console.log(list.length + " / " + data.total);
+                }
+            } else if (response.status === 429 && response.headers.has("retry-after")) {
+                let retryDelay = parseInt(response.headers.get("retry-after"));
+                console.log(`Retrying after ${retryDelay} seconds...`);
+                // Delay is returned in seconds, we need it in millisec
+                await delay(retryDelay * 1000);
+            }
+        } catch (err) {
+            console.log("Error!!!");
+            console.error(err);
+
+            if (err && err.message) {
+                console.log("Error message: " + err.message);
+            }
         }
-      } catch (err) {
-        console.log("Error!!!");
-        console.error(err);
-  
-        if (err && err.message) {
-          console.log("Error message: " + err.message);
-        }
-      }
     } while (next);
-  
+
     return list;
 };
 
@@ -207,8 +207,8 @@ const GetCurrentlyPlaying = async (name) => {
     let response = await fetch("https://api.spotify.com/v1/me/player", {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Accept': 'application/json'
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
         }
     });
 
@@ -221,8 +221,8 @@ const GetCurrentTrack = async () => {
     let response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
         method: 'GET',
         headers: {
-          'Authorization': `Bearer ${accessToken}`,
-          'Accept': 'application/json'
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/json'
         }
     });
 
@@ -231,14 +231,14 @@ const GetCurrentTrack = async () => {
 };
 
 module.exports = {
-  RefreshToken,
-  GetPlaylist,
-  AddSongsToPlaylist,
-  GetPlaylistTracks,
-  Search,
-  GetUserProfile,
-  GetDevices,
-  Play,
-  GetCurrentlyPlaying,
-  GetCurrentTrack
+    RefreshToken,
+    GetPlaylist,
+    AddSongsToPlaylist,
+    GetPlaylistTracks,
+    Search,
+    GetUserProfile,
+    GetDevices,
+    Play,
+    GetCurrentlyPlaying,
+    GetCurrentTrack
 };
